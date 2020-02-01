951df95e2ab3c73662afa0e200148f8cf2e5d709
slzander <staceyzander@gmail.com>
2020-01-31 22:15:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function findMinAndRemove(array){
-
+    let min = 0
+    for(let i = 1; i < array.length; i++){
+        if(array[i] < array[min]){
+            min = i
+        }
+    }
+    return array.splice(min, 1)[0]
 }
 
 function insertionSort(array){
-
+    let newArray = []
+    while(array.length > 0){
+        newArray.push(findMinAndRemove(array))
+    }
+    return newArray
 }
